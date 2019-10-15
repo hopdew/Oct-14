602273ba3ec8435a5e803977356e67f813158d4a
100102324 <hopdeww@gmail.co>
2019-10-15 08:46:42 -0500

    practiced accumulators and stuff. got a lottt of questions and problems to do and also dont forget to put reasons as to why they work that way

--- MathMathMathMarh.py
+++ MathMathMathMarh.py
@@ -24,4 +24,23 @@ for sides in range (8, 100, 8):
 # Modfiy the Archimeds function to tale the radius of the circle as a parameter
 #  Can you get a better answer more quickly using a larger circle?
 
+# Accumulators
 
+acc = 0
+for x in range(1, 6):
+    acc = acc + x
+
+print(acc)
+
+# compute the sum of the first 100 even numbers
+# compute the sim of the first 50 odd numbers
+# compute the average of the first 100 odd numbers
+# write a function that returns the average of the first N numbers, where
+#   N is a parameter
+# write a function called factorial that computes the product of the first
+#    N numbers, where N is a parameter
+# Each number in the Fibonacci sequence id the sim of the previous two numbers
+#   The first two numbers in the sequence are 1 and 1. compute the 10th Fibonacci
+#   number.
+# Write a function to compute the Nth Fibonacci number, where N is a parameter
+#   You may assume that N will be greater than or equal to 3.
